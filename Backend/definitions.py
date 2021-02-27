7b0ae3644a7ca676c131c6fadd0611fbaedfbcf2
apangasa <60531810+apangasa@users.noreply.github.com>
2021-02-27 01:58:24 -0500

    Create functions to make each species based on weight and color distributions.
    Also added map from species keys to function pointers to these functions.

--- Backend/definitions.py
+++ Backend/definitions.py
@@ -39,7 +39,7 @@ class Bird(Animal):
 
 class Monkey(Mammal):
     key = 0
-    colors = []
+    colors = ['brown', 'red', 'orange', 'yellow', 'white', 'grey', 'black']
 
     def __init__(self, attributes):
         self.weight = attributes[0]
@@ -48,7 +48,7 @@ class Monkey(Mammal):
 
 class Lion(Mammal):
     key = 1
-    colors = []
+    colors = ['yellow', 'brown', 'orange', 'white']
 
     def __init__(self, attributes):
         self.weight = attributes[0]
@@ -57,7 +57,7 @@ class Lion(Mammal):
 
 class Hippo(Mammal):
     key = 2
-    colors = []
+    colors = ['brown', 'grey']
 
     def __init__(self, attributes):
         self.weight = attributes[0]
@@ -66,7 +66,7 @@ class Hippo(Mammal):
 
 class Crocodile(ReptileAmph):
     key = 3
-    colors = []
+    colors = ['grey', 'black', 'dark green', 'brown']
 
     def __init__(self, attributes):
         self.weight = attributes[0]
@@ -75,7 +75,7 @@ class Crocodile(ReptileAmph):
 
 class Snake(ReptileAmph):
     key = 4
-    colors = []
+    colors = ['yellow', 'dark green', 'brown', 'black']
 
     def __init__(self, attributes):
         self.weight = attributes[0]
@@ -84,7 +84,8 @@ class Snake(ReptileAmph):
 
 class Frog(ReptileAmph):
     key = 5
-    colors = []
+    colors = ['red', 'yellow', 'orange', 'dark green', 'dark blue',
+              'light green', 'light blue', 'purple', 'pink', 'white', 'grey', 'black']
 
     def __init__(self, attributes):
         self.weight = attributes[0]
@@ -93,7 +94,8 @@ class Frog(ReptileAmph):
 
 class Parrot(Bird):
     key = 6
-    colors = []
+    colors = ['red', 'yellow', 'orange', 'dark green', 'dark blue',
+              'light green', 'light blue', 'purple', 'pink', 'white', 'grey', 'black']
 
     def __init__(self, attributes):
         self.weight = attributes[0]
@@ -102,7 +104,7 @@ class Parrot(Bird):
 
 class Duck(Bird):
     key = 7
-    colors = []
+    colors = ['white', 'yellow']
 
     def __init__(self, attributes):
         self.weight = attributes[0]
@@ -111,7 +113,7 @@ class Duck(Bird):
 
 class Seagull(Bird):
     key = 8
-    colors = []
+    colors = ['white', 'grey']
 
     def __init__(self, attributes):
         self.weight = attributes[0]
@@ -134,3 +136,56 @@ def make_animal(weight_mean, weight_sd, r_mean, g_mean, b_mean, color_sd, grey_s
             # make R = G = B to produce grey color
             color = (col_val, col_val, col_val)
     return (weight, color)
+
+
+def make_monkey():
+    return Monkey(make_animal(50, 22, 147, 82, 32, 20))
+
+
+def make_lion():
+    return Lion(make_animal(350, 50, 222, 204, 156, 5))
+
+
+def make_hippo():
+    return Hippo(make_animal(3500, 250, 100, 100, 100, 50, grey_scale=True))
+
+
+def make_crocodile():
+    return Crocodile(make_animal(1000, 500, 67, 96, 0, 15))
+
+
+def make_snake():
+    return Snake(make_animal(60, 30, 67, 96, 0, 15))
+
+
+def make_frog():
+    return Frog(make_animal(0.5, 0.5, 150, 150, 150, 100))
+
+
+def make_parrot():
+    return Parrot(make_animal(3, 2, 150, 150, 150, 100))
+
+
+def make_duck():
+    return Duck(make_animal(2.5, 0.5, 255, 221, 89, 5))
+
+
+def make_seagull():
+    return Seagull(make_animal(2, 1, 225, 225, 225, 25, grey_scale=True))
+
+
+mappings = {
+    0: make_monkey,
+    1: make_lion,
+    2: make_hippo,
+    3: make_crocodile,
+    4: make_snake,
+    5: make_frog,
+    6: make_parrot,
+    7: make_duck,
+    8: make_seagull
+}
+
+color_rgbs = {
+
+}
