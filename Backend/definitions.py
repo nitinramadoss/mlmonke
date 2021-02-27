13cf1231ad1b46e469755b40925cf7027fdec038
apangasa <60531810+apangasa@users.noreply.github.com>
2021-02-26 19:23:12 -0500

    Create Subclasses of Animal for each Category and Species
    Need to add list of colors corresponding to each Animal species.

--- Backend/definitions.py
+++ Backend/definitions.py
@@ -14,3 +14,102 @@ class Animal:
 
     def get_blue(self):
         return self.color[2]
+
+
+class Mammal(Animal):
+    def __init__(self, attributes):
+        self.weight = attributes[0]
+        self.color = attributes[1]
+
+
+class ReptileAmph(Animal):
+    def __init__(self, attributes):
+        self.weight = attributes[0]
+        self.color = attributes[1]
+
+
+class Bird(Animal):
+    def __init__(self, attributes):
+        self.weight = attributes[0]
+        self.color = attributes[1]
+
+
+class Monkey(Mammal):
+    key = 0
+    colors = []
+
+    def __init__(self, attributes):
+        self.weight = attributes[0]
+        self.color = attributes[1]
+
+
+class Lion(Mammal):
+    key = 1
+    colors = []
+
+    def __init__(self, attributes):
+        self.weight = attributes[0]
+        self.color = attributes[1]
+
+
+class Hippo(Mammal):
+    key = 2
+    colors = []
+
+    def __init__(self, attributes):
+        self.weight = attributes[0]
+        self.color = attributes[1]
+
+
+class Crocodile(ReptileAmph):
+    key = 3
+    colors = []
+
+    def __init__(self, attributes):
+        self.weight = attributes[0]
+        self.color = attributes[1]
+
+
+class Snake(ReptileAmph):
+    key = 4
+    colors = []
+
+    def __init__(self, attributes):
+        self.weight = attributes[0]
+        self.color = attributes[1]
+
+
+class Frog(ReptileAmph):
+    key = 5
+    colors = []
+
+    def __init__(self, attributes):
+        self.weight = attributes[0]
+        self.color = attributes[1]
+
+
+class Parrot(Bird):
+    key = 6
+    colors = []
+
+    def __init__(self, attributes):
+        self.weight = attributes[0]
+        self.color = attributes[1]
+
+
+class Duck(Bird):
+    key = 7
+    colors = []
+
+    def __init__(self, attributes):
+        self.weight = attributes[0]
+        self.color = attributes[1]
+
+
+class Seagull(Bird):
+    key = 8
+    colors = []
+
+    def __init__(self, attributes):
+        self.weight = attributes[0]
+        self.color = attributes[1]
